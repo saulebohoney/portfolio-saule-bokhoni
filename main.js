a3943b4d2f161fe42dbd8f1133b674c369beda59
Saule BN <saulebohoney@gmail.com>
2017-09-15 10:12:39 -0400

    main.js added

---
+++ main.js
@@ -0,0 +1,12 @@
+
+  function fadeIn () {
+    console.log("hello!!!!!!");
+   $(this).closest('.info').addClass('fadeIn').removeClass('hidden');
+  }
+
+  function fadeOut () {
+    console.log("hello!");
+    $(this).closest('.info').addClass('fadeOut');
+   }
+
+  
